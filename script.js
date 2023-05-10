c70225f87c476202c762404244f042399e43dd91
sajwarkhan <sajwarkhan@gmail.com>
2023-05-10 15:59:24 +0500

    changes

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-
+class Circle{
+    constructor (r){
+        this.radius = r
+    }
+    areaOf () {
+        return 2*3.14*this.radius
+    }
+}
+let ob = new Circle(5)
+let a = ob.areaOf()
+console.log(a);
+let ob2 = new Circle(10)
+let b = ob2.areaOf()
+console.log(b);
+let ob3 = new Circle(15)
+let c = ob3.areaOf()
+console.log(ob3);
 
 let x= 2+5
 console.log(x)

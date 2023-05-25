0e5da922dfe85c619a0ff17182322690d47e805f
sajwarkhan <sajwarkhan@gmail.com>
2023-05-25 14:34:51 +0500

    newlecture

---
+++ coddingcallenge.js
@@ -0,0 +1,125 @@
+-----------------------------------
+/* New Lecture
+
+// let js='amazing';
+// if(js==='amazing') alert ('javaScript is Fun!!')
+// console.log(43+30-8);
+
+// let myFirsstName= 'sajwar';
+// let myNumber= 22;
+
+// console.log(myNumber);
+// console.log(myFirsstName);
+/*
+
+// --------------------------------------
+    //   Assignment
+
+      let country= ('pakistan');
+      let continent=('asia');
+      let population=(250000000);
+
+      console.log(country);
+      console.log(continent);
+      console.log(population);
+      */
+
+      /*
+    //   Data Types
+     let java=('year')
+     console.log(typeof java);
+     
+     java=(20);
+     console.log(typeof java);
+
+     java=(true);
+     console.log(typeof java);
+     
+     java=(null);
+     console.log(typeof java);
+     */
+    
+
+//  --------------------------------
+
+// Codding challege
+
+
+
+// let markMass= (78);
+// let markHeight= (1.69);
+
+
+// let johnMass= (92);
+// let johnHeight= (1.95);
+
+// let markBmi=(markMass/ (markHeight * markHeight));
+
+// console.log(markBmi);
+
+// let johnBmi= (johnMass/ (johnHeight * johnHeight))
+
+// console.log(johnBmi);
+
+// let higherBmi=(markBmi<johnBmi)
+// console.log(higherBmi)
+
+
+// markMass= 95;
+// markHeight= 1.88;
+// johnMass=85;
+// johnHeight=1.76;
+
+// markBmi= markMass/ markHeight **2;
+// johnBmi=johnMass/ johnHeight **2;
+// console.log(markBmi,johnBmi);
+
+// higherBmi=markBmi<johnBmi;
+// console.log(higherBmi);
+
+// ------------------------------------
+
+// Codding Challege 2
+
+// let markMass= 95;
+// let markHeight= 1.88;
+// let johnMass=85;
+// let johnHeight=1.76;
+
+// let markBmi = markMass/ markHeight **2;
+// let johnBmi=johnMass/ johnHeight **2;
+// // higherBmi=markBmi<johnBmi;
+// // console.log(higherBmi)
+// if (markBmi>johnBmi)
+// {console.log(`john's Bmi is higher than Mark's Bmi`)
+// }
+
+// else{
+//   console.log(`John's Bmi ( ${johnBmi} ) is higher than the Mark's Bmi ( ${markBmi} )`)
+
+  
+// }
+
+
+// ---------------------------------------------
+
+// Codding Challenge 3
+
+
+// let dolphinsScoreAvg= (96+108+89)/3;
+
+// let koalasScoreAvg=(88+91+110)/3 ;
+
+// console.log('Dolphins score average ' +dolphinsScoreAvg, ' Koalas score average'+koalasScoreAvg)
+
+
+// if(dolphinsScoreAvg>koalasScoreAvg)
+  
+// console.log("The winner of the competition are Dolphins");
+
+// else if(koalasScoreAvg>dolphinsScoreAvg)
+//   console.log("the winner of the competitions are Koalas");
+
+// else if (dolphinsScoreAvg==koalasScoreAvg)
+//   console.log('the match is draw');
+

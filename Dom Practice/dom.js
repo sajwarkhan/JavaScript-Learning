2d3502c06371319d9564f390173064bc56fb188b
sajwarkhan <sajwarkhan@gmail.com>
2023-05-25 14:24:22 +0500

    new commit

--- Dom Practice/dom.js
+++ Dom Practice/dom.js
@@ -1,19 +1,23 @@
-const modal = document.querySelector(".modal")
-const openBtn = document.querySelector(".modal-btn");
-const closeBtn = document.querySelector(".close-btn");
-const backdrop = document.querySelector("#backdrop");
+// const modal = document.querySelector(".modal")
+// const openBtn = document.querySelector(".modal-btn");
+// const closeBtn = document.querySelector(".close-btn");
+// const backdrop = document.querySelector("#backdrop");
 
 
 
-openBtn.addEventListener("click", function(){
-    modal.style.display = "block";
-    backdrop.style.display = "block";
-})
-closeBtn.addEventListener("click", Close)
-backdrop.addEventListener("click",Close)
+// openBtn.addEventListener("click", function(){
+//     modal.style.display = "block";
+//     backdrop.style.display = "block";
+// })
+// closeBtn.addEventListener("click", Close)
+// backdrop.addEventListener("click",Close)
 
-function Close(){
-    modal.style.display = "none";
-    backdrop.style.display = "none"; 
-}
+// function Close(){
+//     modal.style.display = "none";
+//     backdrop.style.display = "none"; 
+// }
+
+let x =[8, 3, 2]
+x.push(x.pop());
+console.log(x);
 

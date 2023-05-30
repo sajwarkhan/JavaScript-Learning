1036655b1c478284a4f132e8c365f41f70adcef8
sajwarkhan <sajwarkhan@gmail.com>
2023-05-30 18:59:31 +0500

    expresssions

--- Functiondeclarations.js
+++ Functiondeclarations.js
@@ -6,4 +6,16 @@ let compJuice = fruits (5,8)
 
 console.log(compJuice)
  
+// functions deleration vs expression
 
+function calculateage1(birthYear){
+return 2023-birthYear;
+}
+// console.log(age1(1995))
+const age1= calculateage1(1995)
+
+calculateage2 =function (birthYear2){
+    return 2023-birthYear2
+}
+const age2 = calculateage2(1994)
+console.log(age1, age2)

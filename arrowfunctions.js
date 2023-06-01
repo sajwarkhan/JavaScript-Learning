7bee9b2675fc4e942cce47cec2d82e8d69b9311c
sajwarkhan <sajwarkhan@gmail.com>
2023-06-01 19:42:46 +0500

    arrowfunction

---
+++ arrowfunctions.js
@@ -0,0 +1,23 @@
+// const ageCalc = function (birthYear){
+//     const age= 2023-birthYear
+//     return `your age is ${age} and your birth Year is ${birthYear}`
+
+// }
+// const year= ageCalc(1995)
+// console.log(year)
+
+
+
+// const ageCalc = birthYear => 2023-birthYear;
+// const age = ageCalc(1995)
+// console.log(age)
+
+
+const ageCalc = (birthYear, firsMan) => {const age= 2023-birthYear;
+return `the ${firstMan} age is ${age}`
+}
+
+console.log(ageCalc (1995, 'Sajwar'))
+console.log(ageCalc (1994, 'Adeesh'))
+
+

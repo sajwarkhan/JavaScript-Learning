393453ebb2a52b5e0b1ee09620d0c0067f1d9c20
sajwarkhan <sajwarkhan@gmail.com>
2023-06-06 16:12:16 +0500

    arrayfirstcommit

---
+++ arrayfirstlec.js
@@ -0,0 +1,8 @@
+const calcAvg = (birthYear)=> expAge=2023-birthYear;
+
+const dOb =[1963, 1961, 1993, 1994,1995,1999]
+const age1= calcAvg(dOb[0])
+const age2= calcAvg(dOb[3])
+const age3= calcAvg(dOb[dOb.length - 1 ])
+console.log (age1, age2, age3)
+

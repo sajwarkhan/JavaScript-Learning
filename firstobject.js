c81bada2e2211d91b31be192d7a763b9b15b49d0
sajwarkhan <sajwarkhan@gmail.com>
2023-06-07 18:44:35 +0500

    first object

---
+++ firstobject.js
@@ -0,0 +1,12 @@
+const jonas = {
+    name: 'jonas',
+    bestFriend : 'micheal',
+    friendsTwo : 'steven',
+    friendThree : 'jack',
+    
+};
+// console.log(jonas.bestFriend)
+const identity = prompt('choose your best friend'
+);
+
+console.log(`${jonas.name} has 3 friends and best friend is ${jonas.bestFriend}`);

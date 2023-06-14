8f2c42b685ca4d79ece2dd444d9d074609f9bc38
sajwarkhan <sajwarkhan@gmail.com>
2023-06-14 20:18:45 +0500

    array

--- Domandeventfundamental/dom.js
+++ Domandeventfundamental/dom.js
@@ -1,3 +1,8 @@
-for(i=1; i>bills; i++){
-    
+const array = [23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33];
+
+const y = [];
+for (i = 0; 1 < 3; i++) {
+  let x = array[i];
+  y.push(x);
 }
+console.log(y);
